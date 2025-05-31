6b7db56e5d538c2beb7dc162cf1555503a25e17f
Aysel84 <ayselgh84@gmail.com>
2025-05-31 19:15:14 +0330

    adding color to the second bottle

--- Bottles.java
+++ Bottles.java
@@ -67,5 +67,23 @@ public class Bottles {
         }
         return  false;
     }
+    public void add(Rectangle rect){
+
+        Rectangle rec = new Rectangle(rect.getX() , rect.getY() , 60 , 50);
+        rec.setFill(rect.getFill());
+        rec.setX(rectangle.getX());
+
+        if(littleRectangles.size() !=  0){
+
+
+            rec.setY(findToppestRect().getY()-50);
+        }
+        else{
+            rec.setY(360);
+        }
+        group.getChildren().remove(rect);
+        littleRectangles.add(rec);
+        group.getChildren().add(rec);
+    }
 
 }
