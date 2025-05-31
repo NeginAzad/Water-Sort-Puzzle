363ccd8020b257b4643c8fe248d4cb71cdf36331
Aysel84 <ayselgh84@gmail.com>
2025-05-31 18:43:36 +0330

    adding variables of game scene method

--- Main.java
+++ Main.java
@@ -18,7 +18,47 @@ import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
+import javafx.scene.text.FontWeight;    
 import javafx.stage.Stage;
 import javafx.util.Duration;
+
+
+public class Main extends Application{
+
+
+
+    public void start(Stage stage) throws Exception{
+
+        
+    } 
+    public void showGameScene(Stage stage , int random){
+
+        Pane pane = new Pane();
+        pane.setStyle("-fx-background-color: linear-gradient(to bottom,rgb(235, 237, 218), #c2e9fb);");
+        Bottles bottle = new Bottles();
+        int[] counter = {0};
+        ColorArrangment colorArrangment = new ColorArrangment();
+        CreateRectangles createRectangles = new CreateRectangles();
+
+        ResetGame resetButton = new ResetGame(pane, createRectangles.bottles, createRectangles,random);
+        Button reset = new Button("RESET");
+
+        /*
+         236 - 365 bahar
+         */
+
+        Rectangle[] toppestRect1 = new Rectangle[1];
+        Rectangle[] toppestRect2 = new Rectangle[1];
+        int[] index1 = new int[1];
+        int[] index2 = new int[1];
+
+
+    }
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+}
+
 
