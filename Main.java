629569ef00c24d818e78975be28ecc35d2adf369
Aysel84 <ayselgh84@gmail.com>
2025-05-31 19:41:20 +0330

    implementing correct arrangment of colors

--- Main.java
+++ Main.java
@@ -58,20 +58,24 @@ public class Main extends Application {
                 createRectangles.bottle();
                 createRectangles.littleRect(colorArrangment.type2());
                 break;
-            case 3:
-                createRectangles.bottle();
-                createRectangles.littleRect(colorArrangment.type3());
-                break;
-            case 4:
-                createRectangles.bottle();
-                createRectangles.littleRect(colorArrangment.type4());
-                break;
-            case 5:
-                createRectangles.bottle();
-                createRectangles.littleRect(colorArrangment.type5());
-                break;
-
-        }
+              case 3:
+                 createRectangles.bottle();
+                 createRectangles.littleRect(colorArrangment.type3());
+                 break;
+             case 4:
+                 createRectangles.bottle();
+                 createRectangles.littleRect(colorArrangment.type4());
+                 break;
+             case 5:
+                 createRectangles.bottle();
+                 createRectangles.littleRect(colorArrangment.type5());
+                 break;
+                 
+            }
+                for(int i = 0 ; i < 6 ; i++){
+        
+                    pane.getChildren().addAll(createRectangles.bottles.get(i).group);
+                }
 
     }
 
