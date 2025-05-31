4783c54962e904fae32e1a787d1478c592c366a4
Aysel84 <ayselgh84@gmail.com>
2025-05-31 18:49:09 +0330

    adding fields of bottles class

---
+++ Bottles.java
@@ -0,0 +1,8 @@
+public class Bottles {
+    
+    private ArrayList<Rectangle> littleRectangles = new ArrayList<>();
+    private Rectangle rectangle = new Rectangle();
+     Group group = new Group();
+     //ایندکس بالاترین مستطیل
+     private int index;
+}
