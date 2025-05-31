73944bc068d7bf761c93227c2f4787b51a922c0f
Negin Azad <ngn.azadkh@gmail.com>
2025-05-31 13:05:34 +0330

    Added classes that Negin has created

---
+++ BackGroundMusic.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+public class BackGroundMusic {
+    public MediaPlayer backGrounMediaPlayer;
+
+    public BackGroundMusic() {
+        String filePath = "E://Coffee-Shop-Song.mp3";
+        File file = new File(filePath);
+
+        if (!file.exists()) {
+            System.out.println("Not valid: " + filePath);
+            return; 
+        }
+
+        String mediaPath = file.toURI().toString();
+
+        try {
+            Media media = new Media(mediaPath);
+            backGrounMediaPlayer = new MediaPlayer(media);
+            backGrounMediaPlayer.setCycleCount(MediaPlayer.INDEFINITE); 
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error loading media");
+        }
+    }
+
+    public void play() {
+        if (backGrounMediaPlayer != null) {
+            backGrounMediaPlayer.play();
+        }
+    }
+
+    
+}
---
+++ MusicPourWater.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+public class MusicPourWater {
+
+    public MediaPlayer mediaPlayer;
+
+    public MusicPourWater() {
+        String filePath = "E:\\water-pill.mp3"; 
+        File file = new File(filePath);
+
+        if (!file.exists()) {
+            System.out.println("Not valid: " + filePath);
+            return; 
+        }
+
+        String mediaPath = file.toURI().toString();
+
+        try {
+            Media media = new Media(mediaPath);
+            mediaPlayer = new MediaPlayer(media);
+            mediaPlayer.setVolume(1.0); 
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error loading media");
+        }
+    }
+
+    public void play() {
+        if (mediaPlayer != null) {
+            mediaPlayer.play();
+        }
+    }
+
+    
+}
