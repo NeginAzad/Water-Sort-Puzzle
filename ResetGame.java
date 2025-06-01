c70588bbacc55edc79249c028f9391341e50ae0b
Negin Azad <ngn.azadkh@gmail.com>
2025-06-01 22:21:22 +0330

    Initialize needed attributes

---
+++ ResetGame.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import javafx.scene.layout.Pane;
+
+public class ResetGame {
+    int random;
+    Bottles bottle = new Bottles();
+    CreateRectangles createRectangles = new CreateRectangles();
+    Pane pane = new Pane();
+    private ArrayList<Bottles> bottles;
+
+    
+}
