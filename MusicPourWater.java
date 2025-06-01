3e3064a1860a1fb8e6566820784d81af1c676f85
Negin Azad <ngn.azadkh@gmail.com>
2025-06-01 22:05:11 +0330

    Add chech if file exists

--- MusicPourWater.java
+++ MusicPourWater.java
@@ -1,12 +1,20 @@
  import java.io.File;
- import javafx.scene.media.Media;
  import javafx.scene.media.MediaPlayer;
 
  public class MusicPourWater {
 
+    public MediaPlayer mediaPlayer;
      
 
      public MusicPourWater() {
+
+        String filePath = "E:\\water-pill.mp3"; 
+         File file = new File(filePath);
+
+         if (!file.exists()) {
+             System.out.println("Not valid: " + filePath);
+             return; 
+         }
          
 
     
