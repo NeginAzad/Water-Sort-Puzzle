cc2337c034f59645312ea2d8b09a58bad76e48c3
Negin Azad <ngn.azadkh@gmail.com>
2025-06-01 21:13:04 +0330

    Implement basic MusicPourWater class with

---
+++ MusicPourWater.java
@@ -0,0 +1,14 @@
+ import java.io.File;
+ import javafx.scene.media.Media;
+ import javafx.scene.media.MediaPlayer;
+
+ public class MusicPourWater {
+
+     
+
+     public MusicPourWater() {
+         
+
+    
+ }
+}
