0a065e6a6d47652dbac58c3790becebcbe3e3ef4
Negin Azad <ngn.azadkh@gmail.com>
2025-07-05 00:06:22 +0330

    Add a class to save the game

---
+++ GameState.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import javafx.scene.paint.Color;
+
+public class GameState {
+    public ArrayList<ArrayList<Color>> colors = new ArrayList<>();
+    public ArrayList<ArrayList<Double>> heights = new ArrayList<>();
+    public ArrayList<ArrayList<Double>> yPositions = new ArrayList<>();
+}
